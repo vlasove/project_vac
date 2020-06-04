eed2016dffeff285c4e9f452030d867e8b394390
EvgenyVlasov <evgeny_vlasov@yahoo.com>
2020-06-04 14:40:20 +0300

    added headers

--- connector/connector.py
+++ connector/connector.py
@@ -1,1 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+
+
+headers = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.142",
+            "accept": "*/*"
+        }
+
