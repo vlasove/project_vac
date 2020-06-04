9e710c72f90db550ec48728fedbf088d23339c09
EvgenyVlasov <evgeny_vlasov@yahoo.com>
2020-06-04 11:48:51 +0300

    add id

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ print(configs)
 conn = sqlite3.connect(configs['path'])
 cur = conn.cursor()
 
-create_table = 'CREATE TABLE IF NOT EXISTS vacancys (id INT PRIMARY KEY, search TEXT, title TEXT, company TEXT, salary INT)'
+create_table = 'CREATE TABLE IF NOT EXISTS vacancys (id INTEGER PRIMARY KEY, search TEXT, title TEXT, company TEXT, salary INT)'
 cur.execute(create_table)
 
 insert_query = 'INSERT INTO vacancys VALUES(NULL, ?, ?, ?, ?)'
 for i in range(10):
-    data = (configs['vacancy'], 'Python Developer', 'Comp' + str(i), 1000 + i*10)
+    data = (configs['vacancy'], 'Java Developer', 'Comp' + str(i), 10000 + i*10)
     cur.execute(insert_query, data)
 
 conn.commit()
